5dfaa019a10b349d44d4fb449421f3c5782b5e02
jonathan <jonathan.ewers123@gmail.com>
2024-12-17 16:20:31 +0000

    Added login. (Not connected to a backend) + Modified structure for scalability.

---
+++ lib/loginPage.dart
@@ -0,0 +1,130 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:json_annotation/json_annotation.dart';
+
+part 'loginPage_g.dart';
+
+@JsonSerializable()
+class FormData {
+  String? email;
+  String? password;
+
+  FormData({
+    this.email,
+    this.password,
+  });
+
+  factory FormData.fromJson(Map<String, dynamic> json) =>
+      _$FormDataFromJson(json);
+
+  Map<String, dynamic> toJson() => _$FormDataToJson(this);
+}
+
+class SignInHttp extends StatefulWidget {
+  const SignInHttp({
+    this.httpClient,
+    super.key,
+    required this.title
+  });
+
+  final http.Client? httpClient;
+  final String title;
+
+  @override
+  State<SignInHttp> createState() => _SignInHttpState();
+}
+
+class _SignInHttpState extends State<SignInHttp> {
+  FormData formData = FormData();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Form(
+        child: Scrollbar(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                ...[
+                  TextFormField(
+                    autofocus: true,
+                    textInputAction: TextInputAction.next,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      hintText: 'Your email address',
+                      labelText: 'Email',
+                    ),
+                    onChanged: (value) {
+                      formData.email = value;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      filled: true,
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                    onChanged: (value) {
+                      formData.password = value;
+                    },
+                  ),
+                  TextButton(
+                    child: const Text('Sign in'),
+                    onPressed: () async {
+                      // Use a JSON encoded string to send
+                      var result = await widget.httpClient!.post(
+                          Uri.parse('https://example.com/signin'),//change to backend
+                          body: json.encode(formData.toJson()),
+                          headers: {'content-type': 'application/json'});
+
+                      _showDialog(switch (result.statusCode) {
+                        200 => 'Successfully signed in.',
+                        401 => 'Unable to sign in.',
+                        _ => 'Something went wrong. Please try again.'
+                      });
+                    },
+                  ),
+                ].expand(
+                      (widget) => [
+                    widget,
+                    const SizedBox(
+                      height: 24,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showDialog(String message) {
+    showDialog<void>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(message),
+        actions: [
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/loginPage_g.dart
@@ -0,0 +1,18 @@
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+part of 'loginPage.dart';
+
+FormData _$FormDataFromJson(Map<String, dynamic> json) {
+  return FormData(
+    email: json['email'] as String?,
+    password: json['password'] as String?,
+  );
+}
+
+Map<String, dynamic> _$FormDataToJson(FormData instance) => <String, dynamic>{
+  'email': instance.email,
+  'password': instance.password,
+};
