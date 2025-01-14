d15fded4cf5c1fc95c6539f3c4df7489c57c900c
Murray Buchanan <murray.buchanan.2022@uni.strath.ac.uk>
2025-01-14 10:46:53 +0000

    Assets

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:task_manager/Controllers/MainController.dart';
 
 import 'package:task_manager/main.dart';
 
