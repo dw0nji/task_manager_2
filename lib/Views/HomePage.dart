1fdda2ab19229cce57ae92ef908add99d43e4cc3
jonathan <jonathan.ewers123@gmail.com>
2025-01-19 20:45:53 +0000

    Restructuring + firebase db

--- lib/Views/HomePage.dart
+++ lib/Views/HomePage.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:task_manager/Model/Task.dart';
@@ -10,7 +12,9 @@ import '../Controllers/MainController.dart';
 
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({super.key, required this.title, required this.current});
+  final String title;
+  final DateTime current;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -49,99 +53,92 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text("Home Page"),
+        title: Text(widget.title),
       ),
       body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints viewportConstraints){
+          builder: (BuildContext context, BoxConstraints viewportConstraints){
 
-          return
-            Stack(
-              children: [
-                Column(
-                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
+            return
+              Stack(
                 children: [
-                  CalendarScrollableSection(),
+                  Column(
+                    //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
-                  Expanded(child:
-                    Consumer<MainController>(
-                      builder: (context, controller, child) {
+                      children: [
+                        CalendarScrollableSection(),
 
-                        // Get the current date and time from the device
-                        DateTime now = DateTime.now();
-                        DateTime fromMidnight = DateTime(now.year, now.month, now.day); // Start of the day at midnight
+                        Expanded(child:
+                        Consumer<MainController>(
+                          builder: (context, controller, child) {
+                            var data = controller.getTasks(widget.current).toList();
+                            data.sort((a, b) => a.date.compareTo(b.date));
+                            //print(data.toString());
 
-                        // Fetch tasks and sort them
-                        var data = controller.getTasks(now).toList();
-                        data.sort((a, b) => a.date.compareTo(b.date));
+                            final List<TimeContainer> timeContainers2 = [];
+                            DateTime fromMidnight = widget.current.copyWith(hour: 0, minute: 0);
+                            //DateTime(0,0,0,0,0,0,0);
 
-                        //print(data.toString());
+                            int counter = 0; // counter for data
+                            for (int i = 0; i < 24; i++) {
+                              DateTime current = fromMidnight.add(Duration(hours: i));
+                              if(counter < data.length && data[counter].date.hour == current.hour) {
+                                TimeContainer container =
+                                TimeContainer(date: current,
+                                    task: UserTaskUI(task:data[counter])
+                                );
+                                timeContainers2.add(container);
 
-                        // Process the tasks into time containers
-                        final List<TimeContainer> timeContainers2 = [];
-                        //DateTime(0,0,0,0,0,0,0);
+                                i--; // after a successfully add, try the same day to see if more exist
+                                counter++;
+                              }else{
+                                TimeContainer container = TimeContainer(date: current);
+                                timeContainers2.add(container);
+                              }
+                            }
 
-                        int counter = 0; // counter for data
-                        for (int i = 0; i < 24; i++) {
-                          DateTime current = fromMidnight.add(Duration(hours: i));
-                          if(counter < data.length && data[counter].date.hour == current.hour) {
-                            TimeContainer container =
-                            TimeContainer(date: current,
-                                task: UserTaskUI(task:data[counter])
-                            );
-                            timeContainers2.add(container);
+                            return SingleChildScrollView(
+                              child: ConstrainedBox(
+                                constraints:
+                                BoxConstraints(minHeight: viewportConstraints.maxHeight),
 
-                            i--; // after a successfull add, try the same day to see if more exist
-                            counter++;
-                          }else{
-                            TimeContainer container = TimeContainer(date: current);
-                            timeContainers2.add(container);
-                          }
-                        }
+                                child:
+                                Center(
 
-                        return SingleChildScrollView(
-                          child: ConstrainedBox(
-                            constraints:
-                            BoxConstraints(minHeight: viewportConstraints.maxHeight),
+                                  child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: timeContainers2
 
-                            child:
-                              Center(
-
-                              child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: timeContainers2
-
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                        );
-                      },
-                    )
-                  )
-                ]
-                ),
-                Container(
-                  alignment: Alignment.bottomRight,
-                  padding: const EdgeInsets.all(20.0),
-                  child: 
-                  FloatingActionButton(
-                    onPressed: taskFormPopup,
-                    //onPressed: (){Provider.of<MainController>(context, listen: false).addTask(Task("hello", "no", DateTime(0,0,0,15,20)));},
-                    child: Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    ),
-                  )
-                ),
-              ],
-          );
-        }
+                            );
+                          },
+                        )
+                        )
+                      ]
+                  ),
+                  Container(
+                      alignment: Alignment.bottomRight,
+                      padding: const EdgeInsets.all(20.0),
+                      child:
+                      FloatingActionButton(
+                        onPressed: taskFormPopup,
+                        //onPressed: (){Provider.of<MainController>(context, listen: false).addTask(Task("hello", "no", DateTime(0,0,0,15,20)));},
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                      )
+                  ),
+                ],
+              );
+          }
       ),
-      );
+    );
   }
 }
 
@@ -188,30 +185,30 @@ class TimeContainerState extends State<TimeContainer>{
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(15.0),
-      padding: const EdgeInsets.all(3.0),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        border: Border(
-          bottom: BorderSide(
-            color: Colors.black12,
-            width: 2.0,
+        margin: const EdgeInsets.all(15.0),
+        padding: const EdgeInsets.all(3.0),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.black12,
+              width: 2.0,
+            ),
           ),
         ),
-      ),
-      child: InkWell(
-        onTap: widget.task != null ? null : handleAddTask,
-        child: Row(
-        //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget> [
-          widget.task != null ? widget.task! :Spacer(),
+        child: InkWell(
+          onTap: widget.task != null ? null : handleAddTask,
+          child: Row(
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget> [
+              widget.task != null ? widget.task! :Spacer(),
 
-          Spacer(),
-          Text(compareNow() ? "NOW" : DateFormat('HH:mm').format(widget.date)),
+              Spacer(),
+              Text(compareNow() ? "NOW" : DateFormat('HH:mm').format(widget.date)),
 
-        ],
-        ),
-      )
+            ],
+          ),
+        )
     );
 
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:task_manager/Views/HomePage.dart';
 import 'package:task_manager/Views/authPage.dart';
@@ -6,146 +7,148 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AuthPage(),
-    );
-  }
-
-}
-
-
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:task_manager/Controllers/DBController.dart';
-// import 'package:task_manager/Views/scaffold.dart';
-//
-// import 'Controllers/MainController.dart';
-// import 'Views/HomePage.dart';
-// import 'Views/ProfilePage.dart';
-// import 'Views/widgets/fade_transition_page.dart';
-// import 'Views/loginPage.dart';
-// import 'package:go_router/go_router.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
-//
-// final appShellNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'app shell');
-//
 // void main() async {
 //   WidgetsFlutterBinding.ensureInitialized();
 //   await Firebase.initializeApp(
 //     options: DefaultFirebaseOptions.currentPlatform,
 //   );
-//   runApp(
-//     ChangeNotifierProvider(
-//       create: (context) => MainController(),
-//       child: const MyApp(),
-//     ),
-//   );
+//   runApp(const MyApp());
 // }
 //
 // class MyApp extends StatelessWidget {
 //   const MyApp({super.key});
 //
-//   ProfilePage get myProfilePage => ProfilePage(title: 'Profile Page');
-//   LoginPage get loginPage => LoginPage();
-//
 //   @override
 //   Widget build(BuildContext context) {
-//     return MaterialApp.router(
-//       title: 'Task Manager App',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-//         useMaterial3: true,
-//       ),
-//       routerConfig: GoRouter(
-//         debugLogDiagnostics: true,
-//         initialLocation: '/home',
-//         routes: [
-//           ShellRoute(
-//             navigatorKey: appShellNavigatorKey,
-//             builder: (context, state, child) {
-//               return TaskManagerScaffold(
-//                 selectedIndex: switch (state.uri.path) {
-//                   var p when p.startsWith('/home') => 0,
-//                   var p when p.startsWith('/profile') => 1,
-//                   _ => 0,
-//                 },
-//                 child: child,
-//               );
-//             },
-//             routes: [
-//               GoRoute(
-//                 path: '/home',
-//                 pageBuilder: (context, state) {
-//                   return FadeTransitionPage<dynamic>(
-//                     key: state.pageKey,
-//                     child: MyHomePage(
-//                       title: "Home Page",
-//                       current: DateTime.now(),
-//                     ),
-//                   );
-//                 },
-//               ),
-//               GoRoute(
-//                 path: "/home/:day",
-//                 pageBuilder: (context, state) {
-//                   var unParsedDay = state.pathParameters["day"];
-//                   try {
-//                     DateTime parsedDay = DateTime.parse(unParsedDay!);
-//                     return FadeTransitionPage<dynamic>(
-//                       key: state.pageKey,
-//                       child: MyHomePage(title: "Home Page", current: parsedDay),
-//                     );
-//                   } catch (e) {
-//                     print("error: $e");
-//                     return FadeTransitionPage<dynamic>(
-//                       key: state.pageKey,
-//                       child: MyHomePage(title: "Home Page", current: DateTime.now()),
-//                     );
-//                   }
-//                 },
-//               ),
-//               GoRoute(
-//                 path: '/profile',
-//                 pageBuilder: (context, state) {
-//                   return FadeTransitionPage<dynamic>(
-//                     key: state.pageKey,
-//                     child: myProfilePage,
-//                   );
-//                 },
-//               ),
-//             ],
-//           ),
-//           GoRoute(
-//             path: '/login',
-//             pageBuilder: (context, state) {
-//               return FadeTransitionPage<dynamic>(
-//                 key: state.pageKey,
-//                 child: loginPage,
-//               );
-//             },
-//           ),
-//         ],
-//       ),
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: AuthPage(),
 //     );
 //   }
+//
 // }
+
+
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:task_manager/Controllers/DBController.dart';
+import 'package:task_manager/Views/scaffold.dart';
+
+import 'Controllers/MainController.dart';
+import 'Views/HomePage.dart';
+import 'Views/ProfilePage.dart';
+import 'Views/widgets/fade_transition_page.dart';
+import 'Views/loginPage.dart';
+import 'package:go_router/go_router.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+final appShellNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'app shell');
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => MainController(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  ProfilePage get myProfilePage => ProfilePage();
+  LoginPage get loginPage => LoginPage(onTap: () {  },);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Task Manager App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      routerConfig: GoRouter(
+        debugLogDiagnostics: true,
+        initialLocation: '/home',
+        routes: [
+          ShellRoute(
+            navigatorKey: appShellNavigatorKey,
+            builder: (context, state, child) {
+              return TaskManagerScaffold(
+                selectedIndex: switch (state.uri.path) {
+                  var p when p.startsWith('/home') => 0,
+                  var p when p.startsWith('/profile') => 1,
+                  _ => 0,
+                },
+                child: child,
+              );
+            },
+            routes: [
+              GoRoute(
+                path: '/home',
+                pageBuilder: (context, state) {
+                  return FadeTransitionPage<dynamic>(
+                    key: state.pageKey,
+                    child: MyHomePage(
+                      title: "Home Page",
+                      current: DateTime.now(),
+                    ),
+                  );
+                },
+              ),
+              GoRoute(
+                path: "/home/:day",
+                pageBuilder: (context, state) {
+                  var unParsedDay = state.pathParameters["day"];
+                  try {
+                    DateTime parsedDay = DateTime.parse(unParsedDay!);
+                    return FadeTransitionPage<dynamic>(
+                      key: state.pageKey,
+                      child: MyHomePage(
+                          title: "Home Page", current: parsedDay
+                      ),
+                    );
+                  } catch (e) {
+                    print("error: $e");
+                    return FadeTransitionPage<dynamic>(
+                      key: state.pageKey,
+                      child: MyHomePage(
+                          title: "Home Page", current: DateTime.now()
+                      ),
+                    );
+                  }
+                },
+              ),
+              GoRoute(
+                path: '/profile',
+                pageBuilder: (context, state) {
+                  return FadeTransitionPage<dynamic>(
+                    key: state.pageKey,
+                    child: myProfilePage,
+                  );
+                },
+              ),
+            ],
+          ),
+          GoRoute(
+            path: '/login',
+            pageBuilder: (context, state) {
+              return FadeTransitionPage<dynamic>(
+                key: state.pageKey,
+                child: loginPage,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
